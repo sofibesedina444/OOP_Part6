c29dc43c72c3a8eb284741fbe7db585dac34c901
salva <salvationordeath@gmail.com>
2022-12-14 16:14:04 +0700

    ООП. ДЗ по уроку №6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean condition = Data.checkData("Wv56h_aal","1993_qwer","1993_qwer");
+        if(condition) {
+            System.out.println("Введенные данные логина и пароля корректны");
+        } else {
+            System.out.println("Введенные данные логина и пароля некорректны");
+        }
     }
 }
